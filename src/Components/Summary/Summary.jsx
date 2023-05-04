462a405e5a450b6708eea385bad9e66997ca6db1
sahilwagh03 <sahilwagh142@gmail.com>
2023-05-04 23:11:41 +0530

    localstorage Added

--- src/Components/Summary/Summary.jsx
+++ src/Components/Summary/Summary.jsx
@@ -21,7 +21,6 @@ export const Summary = () => {
                 {show.image && <img className='img-main1' src={show.image.medium} alt={show.name} />}
                 <h2>Summary</h2>
                 <div className='summary' dangerouslySetInnerHTML={{ __html: show.summary }}></div>
-                {/* <p>{show.summary}</p> */}
                 <h2 className='h2'>Book Tickets Here</h2>
                 <Link to={`bookingTicket`} className="btn2">Book Ticket</Link>
             </div>
